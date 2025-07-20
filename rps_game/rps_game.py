3f6523157a181ff48984bf8b51ac3851f36eca06
Zarina <zarina_nugmanova@mail.ru>
2025-07-20 12:14:03 -0400

    moved game to a function and added recursion

--- rps_game/rps_game.py
+++ rps_game/rps_game.py
@@ -2,25 +2,24 @@ import sys
 import random
 from enum import Enum
 
-class RPS(Enum):
-    ROCK = 1
-    PAPER = 2
-    SCISSORS = 3
+def play_rps():
 
-play_again = True
+    class RPS(Enum):
+        ROCK = 1
+        PAPER = 2
+        SCISSORS = 3
 
-while play_again: 
+    player_choice = int(input("\n\nEnter 1 for Rock\nEnter 2 for Paper\nEnter 3 for Scissors:\n\n"))
 
-    player_choice = int(input("Hello\n\nEnter 1 for Rock\nEnter 2 for Paper\nEnter 3 for Scissors:\n\n"))
-
-    if player_choice < 1 or player_choice > 3:
-        sys.exit("You must enter 1, 2 or 3")
-
-    print(f"Your choice is {str(RPS(player_choice)).replace('RPS.', '')}.")
+    if player_choice not in [1, 2, 3]:
+        print("You must enter 1, 2 or 3\n\n")
+        return play_rps()
 
     computer_choice = random.randint(1, 3)
 
+    print(f"Your choice is {str(RPS(player_choice)).replace('RPS.', '')}.")
     print(f"Computer choice is {str(RPS(computer_choice)).replace('RPS.', '')}.")
+
     you_win = "🎉 You win !"
     tie_game = "😊 It's a tie !"
     computer_win = "💻 Computer wins !"
@@ -37,14 +36,20 @@ while play_again:
     else:
         print(computer_win)
 
-    play_again = input('\nEnter Y to keep playing,\nEnter Q to quit\n\n')
+  
+    while True:
+        players_answer = input('\nEnter Y to keep playing,\nEnter Q to quit\n\n')
+        if players_answer.lower() not in ['y', 'q']:
+            continue
+        else:
+            break
 
-    if play_again.lower() == "y":
-        continue
-    elif play_again.lower() == 'q':
-        sys.exit("Thank you for the game,\nBye 👋 !")
+    if players_answer.lower() == "y":
+        return play_rps()
     else:
-        play_again = input('\nEnter Y to keep playing,\nEnter Q to quit\n')
-        
+        sys.exit("\nThank you for the game,\nBye 👋 !")
+  
+            
 
   
+play_rps()
